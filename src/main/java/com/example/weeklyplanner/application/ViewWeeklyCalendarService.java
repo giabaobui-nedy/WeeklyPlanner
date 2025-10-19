dc95dfd160259c9b2157d6a0283677b6a8deef8e
giabaobui36 <100818088+giabaobui36@users.noreply.github.com>
2025-10-19 23:49:43 +1100

    remove extra method in ViewCalendarService

--- src/main/java/com/example/weeklyplanner/application/ViewWeeklyCalendarService.java
+++ src/main/java/com/example/weeklyplanner/application/ViewWeeklyCalendarService.java
@@ -1,17 +1,10 @@
 package com.example.weeklyplanner.application;
 
-import java.util.List;
-
 import com.example.weeklyplanner.domain.model.ISOWeek;
-import com.example.weeklyplanner.domain.model.PlanEntry;
 import com.example.weeklyplanner.domain.model.WeeklyPlan;
 
 public class ViewWeeklyCalendarService {
     public WeeklyPlan getWeeklyPlan(ISOWeek week) {
-        throw new UnsupportedOperationException("Not implemented");
-    }
-
-    public List<PlanEntry> getPlanEntries(WeeklyPlan plan) {
         throw new UnsupportedOperationException("Not implemented");
     }
 }
